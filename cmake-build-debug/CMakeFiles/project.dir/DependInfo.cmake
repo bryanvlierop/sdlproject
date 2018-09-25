b6ce875f5408dfeacce865e5bab5e22f9c2fc59e
Bryan van Lierop <bryanvlierop96@gmail.com>
2018-09-25 13:18:31 +0200

    project init

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bryanvanlierop/CLionProjects/project/main.cpp" "/Users/bryanvanlierop/CLionProjects/project/cmake-build-debug/CMakeFiles/project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/bryanvanlierop/.hunter/_Base/70d75a2/b7ab528/63b5ff1/Install/include"
+  "/Users/bryanvanlierop/.hunter/_Base/70d75a2/b7ab528/63b5ff1/Install/include/SDL2"
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
